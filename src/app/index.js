319087f19b58a6f0007dc0ace3e3d96e6f1a84d9
Unknown <alexlotte16@gmail.com>
2019-02-10 00:08:45 -0500

    build

--- src/app/index.js
+++ src/app/index.js
@@ -4,10 +4,7 @@ import {view} from './view'
 
 // Initialize the app
 app({
-  init: {
-    ...init,
-    ...JSON.parse(window.localStorage.getItem('hyperapp-todolist'))
-  },
+  init: {...init, ...JSON.parse(window.localStorage.getItem('hyperapp-todolist'))},
   view,
   subscriptions: state => window.localStorage.setItem('hyperapp-todolist', JSON.stringify(state)),
   container: document.body
@@ -15,5 +12,5 @@ app({
 
 // Register service worker for offline support
 if ('serviceWorker' in navigator) {
-  // window.addEventListener('load', () => navigator.serviceWorker.register('../service-worker.js'))
+  window.addEventListener('load', () => navigator.serviceWorker.register('../service-worker.js'))
 }
