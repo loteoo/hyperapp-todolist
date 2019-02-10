6ccb12c8f0a7ba2064a3f439537d2eb46f20ff9d
Unknown <alexlotte16@gmail.com>
2019-02-09 23:44:54 -0500

    fix build

--- src/app/index.js
+++ src/app/index.js
@@ -14,7 +14,6 @@ app({
 })
 
 // Register service worker for offline support
-
-// if ('serviceWorker' in navigator) {
-//   window.addEventListener('load', () => navigator.serviceWorker.register('../service-worker.js'))
-// }
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => navigator.serviceWorker.register('../service-worker.js'))
+}
