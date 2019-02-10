30d0b9ff6527157f04f45433b0e4ccb363dbb059
Unknown <alexlotte16@gmail.com>
2019-02-09 23:41:09 -0500

    offline support

--- src/app/index.js
+++ src/app/index.js
@@ -3,4 +3,18 @@ import {init} from './init'
 import {view} from './view'
 
 // Initialize the app
-app({init, view, container: document.body})
+app({
+  init: {
+    ...init,
+    ...JSON.parse(window.localStorage.getItem('hyperapp-todolist'))
+  },
+  view,
+  subscriptions: state => window.localStorage.setItem('hyperapp-todolist', JSON.stringify(state)),
+  container: document.body
+})
+
+// Register service worker for offline support
+
+// if ('serviceWorker' in navigator) {
+//   window.addEventListener('load', () => navigator.serviceWorker.register('../service-worker.js'))
+// }
