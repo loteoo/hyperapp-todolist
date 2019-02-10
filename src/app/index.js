afa839a3b3fba72e20776fc491fdd5f9b964d045
Unknown <alexlotte16@gmail.com>
2019-02-09 23:53:26 -0500

    icons

--- src/app/index.js
+++ src/app/index.js
@@ -15,5 +15,5 @@ app({
 
 // Register service worker for offline support
 if ('serviceWorker' in navigator) {
-  window.addEventListener('load', () => navigator.serviceWorker.register('../service-worker.js'))
+  // window.addEventListener('load', () => navigator.serviceWorker.register('../service-worker.js'))
 }
