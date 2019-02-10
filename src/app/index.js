fc9147789be9f326027b14e8aa32d97aba514c60
Unknown <alexlotte16@gmail.com>
2019-02-10 00:59:45 -0500

    rm sw onload

--- src/app/index.js
+++ src/app/index.js
@@ -12,5 +12,5 @@ app({
 
 // Register service worker for offline support
 if ('serviceWorker' in navigator) {
-  window.addEventListener('load', () => navigator.serviceWorker.register('../service-worker.js'))
+  navigator.serviceWorker.register('../service-worker.js')
 }
